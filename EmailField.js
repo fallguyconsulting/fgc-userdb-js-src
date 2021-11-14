8b43f3b0b84d9ecc5939f9d0f9eb50abf1ab0c78
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-11-13 17:05:37 -0800

    phone and email field really should be here after all

---
+++ EmailField.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-whitespace-before-property */
+
+import * as consts                                      from 'consts';
+import { assert, hooks, RevocableContext }              from 'fgc';
+import { action, computed, observable, runInAction }    from 'mobx';
+import { observer }                                     from 'mobx-react';
+import React, { useState }                              from 'react';
+import * as UI                                          from 'semantic-ui-react';
+import validator                                        from 'validator';
+
+//================================================================//
+// EmailField
+//================================================================//
+export const EmailField = observer (( props ) => {
+
+    const { onEmail, ...rest }      = props;
+
+    const [ email, setEmail ]       = useState ( '' );
+    const [ error, setError ]       = useState ( '' );
+
+    const onChange = ( event ) => {
+        setError ( '' );
+        onEmail ( '' );
+        setEmail ( event.target.value );
+    }
+
+    const onBlur = () => {
+        if ( email && validator.isEmail ( email )) {
+            onEmail ( email );
+        }
+        else {
+            setError ( 'Please enter a valid email address.' );
+        }
+    };
+
+    const onKeyPress = ( event ) => {
+        if ( event.key === 'Enter' ) {
+            event.target.blur ();
+        }
+    }
+
+    return (
+        <UI.Form.Input
+            
+            icon            = 'mail'
+            iconPosition    = 'left'
+            placeholder     = 'Email'
+
+            { ...rest }
+
+            type            = 'string'
+            value           = { email }
+            onChange        = { onChange }
+            onKeyPress      = { onKeyPress }
+            onBlur          = { onBlur }
+            error           = { error || false }
+        />
+    );
+});
--- export.js
+++ export.js
@@ -4,10 +4,12 @@ module.exports = {
     roles:                          require ( './roles.js' ),
     token:                          require ( './token.js' ),
 
+    EmailField:                     require ( './EmailField.js' ).EmailField,
     LogInController:                require ( './LogInController.js' ).LogInController,
     LogInModal:                     require ( './LogInModal.js' ).LogInModal,
     LogInWithCreateUserScreen:      require ( './LogInWithCreateUserScreen.js' ).LogInWithCreateUserScreen,
     LogInWithResetPasswordScreen:   require ( './LogInWithResetPasswordScreen.js' ).LogInWithResetPasswordScreen,
+    PhoneField:                     require ( './PhoneField.js' ).PhoneField,
     SessionController:              require ( './SessionController.js' ).SessionController,
     UserAccountPopup:               require ( './UserAccountPopup.js' ).UserAccountPopup,
     UserManagementController:       require ( './UserManagementController.js' ).UserManagementController,
