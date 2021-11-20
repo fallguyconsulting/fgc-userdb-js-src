44fc281ae37bb11fb0228a86c663864edbb26f0a
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2021-11-19 21:06:09 -0800

    removed unnecessary finalizers

--- UserManagementController.js
+++ UserManagementController.js
@@ -23,12 +23,6 @@ export class UserManagementController {
     }
 
     //----------------------------------------------------------------//
-    finalize () {
-
-        this.revocable.finalize ();
-    }
-
-    //----------------------------------------------------------------//
     @action
     async getUsersAsync () {
         
